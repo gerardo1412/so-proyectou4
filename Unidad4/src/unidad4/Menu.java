366d96c3eb9c181d2a6346e8a7288b15b72b0060
Unknown <maxhg.96@gmail.com>
2019-07-09 16:42:14 -0400

    Creación de menu
    Añadida la funcionalidad de menu. Este debe de ser la primera interfaz con la que el usuario debe de interactuar.

--- Unidad4/src/unidad4/Menu.java
+++ Unidad4/src/unidad4/Menu.java
@@ -5,10 +5,57 @@
  */
 package unidad4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Maximo Hernandez
  */
 public class Menu {
+    
+    
+    public Menu(){
+    }
+    
+    public int menu(){
+        Scanner scanner = new Scanner(System.in);
+        String atributo = "0";
+        int bandera = 0;
+        while (bandera==0){
+            switch(atributo){
+                case "0":
+                    System.out.println("Menu\n(1) para crear disco\n(2) para crear un archivo"
+                            + "\n(3) para remover un archivo\n(4) para abrir un archivo"
+                            + "\n(5) para leer un archivo\n(6) para escribir dentro de un archivo"
+                            + "\n(7) para imprimir lo que esta dentro de un archivo"
+                            + "\n(8) para listar los archivos\n(9) para salir"
+                            + "\nIngrese un numero: ");   
+                    atributo = scanner.next();
+                    break;
+                case "1":
+                    return 1;   
+                case "2":
+                    return 2;
+                case "3":
+                    return 3;
+                case "4":
+                    return 4;
+                case "5":
+                    return 5;
+                case "6":
+                    return 6;
+                case "7":
+                    return 7;
+                case "8":
+                    return 8;
+                case "9":
+                    return 9;
+                default:
+                    System.out.println("Error fatal, cerrando programa.");
+                    return 9;
+            }
+        }
+        return 0;
+    }
     
 }
